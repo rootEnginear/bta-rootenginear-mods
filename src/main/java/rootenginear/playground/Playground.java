80c21f6bbd9819376e6b8c8570ecb6f2d3a6cf76
rootEnginear <rootEnginear@users.noreply.github.com>
2023-10-14 01:33:45 +0700

    chore: scaffolding common structure

--- src/main/java/rootenginear/playground/Playground.java
+++ src/main/java/rootenginear/playground/Playground.java
@@ -1,16 +1,15 @@
-package turniplabs.examplemod;
+package rootenginear.playground;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-public class ExampleMod implements ModInitializer {
-    public static final String MOD_ID = "examplemod";
+public class Playground implements ModInitializer {
+    public static final String MOD_ID = "playground";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        LOGGER.info("ExampleMod initialized.");
+        LOGGER.info("Playground initialized.");
     }
 }
