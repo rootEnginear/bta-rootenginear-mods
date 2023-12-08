33efa8fe3a511ad1c53c62d18e8aefc813eabf3a
rootEnginear <rootEnginear@users.noreply.github.com>
2023-12-09 02:20:11 +0700

    feat: second proto

---
+++ src/main/java/rootenginear/playground/ChunkProcessor.java
@@ -0,0 +1,52 @@
+package rootenginear.playground;
+
+import net.minecraft.core.world.chunk.Chunk;
+
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ChunkProcessor {
+	public static void readAndDumpChunkData(int targetChunkX, int targetChunkZ, Chunk chunk) throws IOException {
+		try (FileOutputStream chunkData = new FileOutputStream(String.format("livemap/chunks/%d.%d", targetChunkX, targetChunkZ))) {
+			for (int shiftZ = 0; shiftZ < 16; shiftZ++) {
+				for (int shiftX = 0; shiftX < 16; shiftX++) {
+					short blockData = 0;
+					for (int y = chunk.getHeightValue(shiftX, shiftZ); y > -1; y--) {
+						int blockId = chunk.getBlockID(shiftX, y, shiftZ);
+						if (blockId != 0) {
+							int metadata = chunk.getBlockMetadata(shiftX, y, shiftZ);
+							blockData = (short) ((blockId & 0x3FF) | ((metadata & 0xF) << 10));
+							break;
+						}
+					}
+					chunkData.write(blockData & 0xFF);
+					chunkData.write((blockData >> 8) & 0xFF);
+				}
+			}
+		}
+	}
+
+	public static void updateChunkFile() throws IOException {
+		try (DirectoryStream<Path> dp = Files.newDirectoryStream(Paths.get("livemap", "chunks"))) {
+			StringBuilder chunkList = new StringBuilder("[");
+			boolean firstPassed = false;
+			for (Path path : dp) {
+				if (firstPassed) {
+					chunkList.append(",");
+				} else {
+					firstPassed = true;
+				}
+				chunkList.append("\"").append(path.getFileName()).append("\"");
+			}
+			chunkList.append("]");
+			try (FileWriter chunkData = new FileWriter("livemap/chunks.json")) {
+				chunkData.write(chunkList.toString());
+			}
+		}
+	}
+}
---
+++ src/main/java/rootenginear/playground/mixin/MinecraftServerMixin.java
@@ -0,0 +1,81 @@
+package rootenginear.playground.mixin;
+
+import net.minecraft.core.entity.player.EntityPlayer;
+import net.minecraft.core.world.chunk.Chunk;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.WorldServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import rootenginear.playground.ChunkProcessor;
+
+import java.io.FileWriter;
+
+@Mixin(value = MinecraftServer.class, remap = false)
+public class MinecraftServerMixin {
+	@Shadow
+	public WorldServer[] worldMngr;
+
+	@Unique
+	final int TPS = 20;
+
+	@Unique
+	final int CHUNK_DUMP_TIME = 30 * TPS;
+
+	@Unique
+	final int PLAYER_DUMP_TIME = 5 * TPS;
+
+	@Unique
+	int currentTick = 0;
+
+	@Inject(method = "doTick", at = @At("TAIL"))
+	void addTime(CallbackInfo ci) {
+		currentTick++;
+	}
+
+	@Inject(method = "doTick", at = @At("TAIL"))
+	void saveChunk(CallbackInfo ci) {
+		if (currentTick % CHUNK_DUMP_TIME != 0) return;
+		try {
+			WorldServer overworld = this.worldMngr[0];
+			for (EntityPlayer player : overworld.players) {
+				int chunkX = (int) player.x / 16;
+				int chunkZ = (int) player.z / 16;
+				for (int chunkShiftX = -1; chunkShiftX < 2; chunkShiftX++) {
+					for (int chunkShiftZ = -1; chunkShiftZ < 2; chunkShiftZ++) {
+						int targetChunkX = chunkX + chunkShiftX;
+						int targetChunkZ = chunkZ + chunkShiftZ;
+						Chunk chunk = overworld.chunkProviderServer.provideChunk(targetChunkX, targetChunkZ);
+						ChunkProcessor.readAndDumpChunkData(targetChunkX, targetChunkZ, chunk);
+						System.out.println("Chunk " + targetChunkX + ", " + targetChunkZ + " dumped!");
+					}
+				}
+			}
+			ChunkProcessor.updateChunkFile();
+		} catch (Exception lol) {
+			System.out.println(lol.toString());
+		}
+	}
+
+	@Inject(method = "doTick", at = @At("TAIL"))
+	void savePlayer(CallbackInfo ci) {
+		if (currentTick % PLAYER_DUMP_TIME != 0) return;
+		try {
+			WorldServer overworld = this.worldMngr[0];
+			StringBuilder playerListJSON = new StringBuilder("{");
+			for (int i = 0; i < overworld.players.size(); i++) {
+				if (i > 0) playerListJSON.append(",");
+				EntityPlayer player = overworld.players.get(i);
+				playerListJSON.append(String.format("\"%s\":[%g,%g]", player.username, player.x, player.z));
+			}
+			playerListJSON.append("}");
+			try (FileWriter chunkData = new FileWriter("livemap/players.json")) {
+				chunkData.write(playerListJSON.toString());
+			}
+		} catch (Exception ignored) {
+		}
+	}
+}
