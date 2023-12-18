9f46c54da757c857c11bcf042cb14df8278e37b7
rootEnginear <rootEnginear@users.noreply.github.com>
2023-12-18 18:31:23 +0700

    feat: 7.1-pre1

---
+++ src/main/java/rootenginear/craftablechainmailarmor/recipe/ChainmailRecipes.java
@@ -0,0 +1,23 @@
+package rootenginear.craftablechainmailarmor.recipe;
+
+import net.minecraft.core.block.Block;
+import net.minecraft.core.data.DataLoader;
+import net.minecraft.core.data.registry.Registries;
+import net.minecraft.core.data.registry.recipe.RecipeGroup;
+import net.minecraft.core.data.registry.recipe.RecipeNamespace;
+import net.minecraft.core.data.registry.recipe.RecipeSymbol;
+import net.minecraft.core.data.registry.recipe.entry.RecipeEntryCrafting;
+import net.minecraft.core.item.ItemStack;
+import turniplabs.halplibe.util.RecipeEntrypoint;
+
+public class ChainmailRecipes implements RecipeEntrypoint {
+	public final RecipeNamespace CHAINMAILMOD = new RecipeNamespace();
+	public final RecipeGroup<RecipeEntryCrafting<?, ?>> WORKBENCH = new RecipeGroup<>(new RecipeSymbol(new ItemStack(Block.workbench)));
+
+	@Override
+	public void onRecipesReady() {
+		CHAINMAILMOD.register("workbench", WORKBENCH);
+		Registries.RECIPES.register("chainmail", this.CHAINMAILMOD);
+		DataLoader.loadRecipes("/assets/craftablechainmailarmor/recipe/workbench.json");
+	}
+}
