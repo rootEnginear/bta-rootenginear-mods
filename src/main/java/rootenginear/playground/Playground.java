7652ac418961ce3beb1907ce945526359686340e
rootEnginear <rootEnginear@users.noreply.github.com>
2024-04-14 21:02:04 +0700

    chore: 7.1 🎉

--- src/main/java/rootenginear/playground/Playground.java
+++ src/main/java/rootenginear/playground/Playground.java
@@ -3,8 +3,10 @@ package rootenginear.playground;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.util.GameStartEntrypoint;
+import turniplabs.halplibe.util.RecipeEntrypoint;
 
-public class Playground implements ModInitializer {
+public class Playground implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
     public static final String MOD_ID = "playground";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
@@ -12,4 +14,13 @@ public class Playground implements ModInitializer {
     public void onInitialize() {
         LOGGER.info("Playground initialized.");
     }
+
+	@Override
+	public void beforeGameStart() {}
+
+	@Override
+	public void afterGameStart() {}
+
+	@Override
+	public void onRecipesReady() {}
 }
