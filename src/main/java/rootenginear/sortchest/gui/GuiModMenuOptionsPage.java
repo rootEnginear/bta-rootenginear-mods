78476d283edc21fedb68079a85f295ed35aae307
rootEnginear <rootEnginear@users.noreply.github.com>
2024-04-14 22:41:24 +0700

    fix: update OptionsPage API

--- src/main/java/rootenginear/sortchest/gui/GuiModMenuOptionsPage.java
+++ src/main/java/rootenginear/sortchest/gui/GuiModMenuOptionsPage.java
@@ -1,25 +1,23 @@
 package rootenginear.sortchest.gui;
 
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.options.GuiOptions;
 import net.minecraft.client.gui.options.components.KeyBindingComponent;
 import net.minecraft.client.gui.options.data.OptionsPage;
 import net.minecraft.client.gui.options.data.OptionsPages;
+import net.minecraft.core.block.Block;
+import net.minecraft.core.item.ItemStack;
 import rootenginear.sortchest.interfaces.ISortChestSettings;
 import turniplabs.halplibe.helper.ModVersionHelper;
 import turniplabs.halplibe.util.ClientStartEntrypoint;
-
-import static rootenginear.sortchest.SortChest.LOGGER;
-
 
 public class GuiModMenuOptionsPage implements ClientStartEntrypoint {
 	public static ISortChestSettings modSettings;
 	public static OptionsPage sortOptions;
 
-	public static GuiOptions getOptionsScreen(GuiScreen parent){
-		return new GuiOptions(parent,  Minecraft.getMinecraft(Minecraft.class).gameSettings, sortOptions);
+	public static GuiOptions getOptionsScreen(GuiScreen parent) {
+		return new GuiOptions(parent);
 	}
 
 	@Override
@@ -30,17 +28,14 @@ public class GuiModMenuOptionsPage implements ClientStartEntrypoint {
 	@Override
 	public void afterClientStart() {
 		modSettings = (ISortChestSettings) Minecraft.getMinecraft(Minecraft.class).gameSettings;
-		sortOptions = new OptionsPage("options.category.sortchest")
+		sortOptions = new OptionsPage("options.category.sortchest", new ItemStack(Block.chestPlanksOak))
 			.withComponent(new KeyBindingComponent(modSettings.bta_rootenginear_mods$getKeyDump()))
 			.withComponent(new KeyBindingComponent(modSettings.bta_rootenginear_mods$getKeyFill()))
 			.withComponent(new KeyBindingComponent(modSettings.bta_rootenginear_mods$getKeyRefill()))
 			.withComponent(new KeyBindingComponent(modSettings.bta_rootenginear_mods$getKeySort()));
 
-
-		if(ModVersionHelper.isModPresent("modmenu")){
+		if (ModVersionHelper.isModPresent("modmenu")) {
 			OptionsPages.register(sortOptions);
 		}
-		//OptionsPages.CONTROLS.withComponent(keybindCategory);
-
 	}
 }
