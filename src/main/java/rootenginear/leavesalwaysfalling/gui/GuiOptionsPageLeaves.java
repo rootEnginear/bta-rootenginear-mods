8276b789f87a3dfe237405ebdd7ee7f0b38f5c30
rootEnginear <rootEnginear@users.noreply.github.com>
2024-04-16 06:45:40 +0700

    fix: migrate modmenu, remove i18n

--- src/main/java/rootenginear/leavesalwaysfalling/gui/GuiOptionsPageLeaves.java
+++ src/main/java/rootenginear/leavesalwaysfalling/gui/GuiOptionsPageLeaves.java
@@ -6,18 +6,33 @@ import net.minecraft.client.gui.options.GuiOptions;
 import net.minecraft.client.gui.options.components.OptionsCategory;
 import net.minecraft.client.gui.options.components.ToggleableOptionComponent;
 import net.minecraft.client.gui.options.data.OptionsPage;
-import net.minecraft.client.option.GameSettings;
+import net.minecraft.client.gui.options.data.OptionsPages;
+import net.minecraft.core.block.Block;
+import net.minecraft.core.item.ItemStack;
 import rootenginear.leavesalwaysfalling.interfaces.ILeavesSettings;
+import turniplabs.halplibe.util.ClientStartEntrypoint;
 
-public class GuiOptionsPageLeaves extends GuiScreen {
-	public static GameSettings gameSettings = Minecraft.getMinecraft(Minecraft.class).gameSettings;
-
-	public static final OptionsPage leavesOptions = new OptionsPage("options.category.leavesalwaysfalling")
-		.withComponent(new OptionsCategory("options.category.leavesalwaysfalling")
-			.withComponent(new ToggleableOptionComponent<>(((ILeavesSettings) gameSettings).bta_rootenginear_mods$getFrequency()))
-		);
+public class GuiOptionsPageLeaves implements ClientStartEntrypoint {
+	public static ILeavesSettings modSettings;
+	public static OptionsPage leavesOptions;
 
 	public static GuiOptions createGui(GuiScreen parent) {
-		return new GuiOptions(parent, gameSettings, leavesOptions);
+		return new GuiOptions(parent, leavesOptions);
+	}
+
+	@Override
+	public void beforeClientStart() {
+
+	}
+
+	@Override
+	public void afterClientStart() {
+		modSettings = (ILeavesSettings) Minecraft.getMinecraft(Minecraft.class).gameSettings;
+		leavesOptions = new OptionsPage("options.category.leavesalwaysfalling", new ItemStack(Block.leavesOak))
+			.withComponent(new OptionsCategory("options.category.leavesalwaysfalling")
+				.withComponent(new ToggleableOptionComponent<>(modSettings.bta_rootenginear_mods$getFrequency()))
+			);
+
+		OptionsPages.register(leavesOptions);
 	}
 }
