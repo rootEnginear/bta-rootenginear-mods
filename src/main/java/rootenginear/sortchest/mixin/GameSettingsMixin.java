ddaa1a0205b349a27b79bae383d11280622b90be
Bigjango13 <bigjango13@gmail.com>
2024-07-12 17:11:26 -0700

    Update to 7.2pr2

--- src/main/java/rootenginear/sortchest/mixin/GameSettingsMixin.java
+++ src/main/java/rootenginear/sortchest/mixin/GameSettingsMixin.java
@@ -2,16 +2,17 @@ package rootenginear.sortchest.mixin;
 
 import net.minecraft.client.option.GameSettings;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.input.InputDevice;
 import org.spongepowered.asm.mixin.Mixin;
 import rootenginear.sortchest.interfaces.ISortChestSettings;
 
 @Mixin(value = GameSettings.class, remap = false)
 public class GameSettingsMixin implements ISortChestSettings {
 	
-	public KeyBinding keyRefill = new KeyBinding("sortchest.refill").bindKeyboard(0x13); //R
-	public KeyBinding keyFill = new KeyBinding("sortchest.fill").bindKeyboard(0x21); //F
-	public KeyBinding keyDump = new KeyBinding("sortchest.dump").bindKeyboard(0x20); //D
-	public KeyBinding keySort = new KeyBinding("sortchest.sort").bindKeyboard(0x1F); //S
+	public KeyBinding keyRefill = new KeyBinding("sortchest.refill").setDefault(InputDevice.keyboard, 0x13); //R
+	public KeyBinding keyFill = new KeyBinding("sortchest.fill").setDefault(InputDevice.keyboard, 0x21); //F
+	public KeyBinding keyDump = new KeyBinding("sortchest.dump").setDefault(InputDevice.keyboard, 0x20); //D
+	public KeyBinding keySort = new KeyBinding("sortchest.sort").setDefault(InputDevice.keyboard, 0x1F); //S
 
 	@Override
 	public KeyBinding bta_rootenginear_mods$getKeySort() {
