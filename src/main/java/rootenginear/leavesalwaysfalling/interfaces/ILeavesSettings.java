6c32a0a6259a97024df6c3356aebca92afcf71a1
rootEnginear <rootenginear@users.noreply.github.com>
2025-05-09 04:16:43 +0700

    fix: mark interface client-side

--- src/main/java/rootenginear/leavesalwaysfalling/interfaces/ILeavesSettings.java
+++ src/main/java/rootenginear/leavesalwaysfalling/interfaces/ILeavesSettings.java
@@ -1,8 +1,11 @@
 package rootenginear.leavesalwaysfalling.interfaces;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.option.OptionFloat;
 import net.minecraft.client.option.OptionRange;
 
+@Environment(EnvType.CLIENT)
 public interface ILeavesSettings {
 	OptionRange bta_rootenginear_mods$getFrequencyOption();
 
